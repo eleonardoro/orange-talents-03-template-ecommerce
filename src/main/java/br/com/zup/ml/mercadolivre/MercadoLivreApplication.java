c3be24c80fe3de6868b86070c69b00dc9bd38046
Eleonardo.Oliveira <eleonardo.oliveira@zup.com.br>
2021-04-05 16:59:24 -0300

    - First Commit

---
+++ src/main/java/br/com/zup/ml/mercadolivre/MercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ml.mercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLivreApplication.class, args);
+	}
+
+}
